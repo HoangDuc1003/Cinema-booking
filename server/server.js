b63f7c03f05df6ad86a0dfe27a6c899312ffa039
armybeaver <hhprolay@gmail.com>
2026-05-07 23:18:16 +0700

    fix connect db to pj

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import dns from "node:dns/promises";
+import { inngest ,functions } from './inngest/index.js';
+import {serve} from 'inngest/express'
+
+//dns
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const app = express();
 const port  = 3000;
@@ -16,5 +22,6 @@ app.use(clerkMiddleware())
 
 //api route 
 app.get('/',(req,res)=>res.send('Server is live!'))
+app.use('api/inngest',serve({client:inngest,functions}))
 app.listen(port, ()=> console.log(`Server is listening at http://localhost:${port}`));
 
