cba91fdda9f5bedfd72418af5f171262e4eed47c
armybeaver <hhprolay@gmail.com>
2026-05-08 18:43:35 +0700

    add showRoutes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import dns from "node:dns/promises";
 import { inngest, functions } from './inngest/index.js';
 import { serve } from 'inngest/express'
+import showRouter from './routes/showRoutes.js';
 
 // Use Cloudflare DNS to avoid resolution issues on serverless
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
@@ -19,15 +20,12 @@ await connectDB()
 // Core middleware
 app.use(express.json())
 app.use(cors())
-
-// Inngest endpoint — MUST be BEFORE clerkMiddleware so webhooks are not blocked
-app.use('/api/inngest', serve({ client: inngest, functions }));
-
-// Clerk auth middleware — applies to all routes AFTER this line
 app.use(clerkMiddleware())
-
-// Health check route
+//api routes
 app.get('/', (req, res) => res.send('Server is live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/show',showRouter)
+
 
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
 
